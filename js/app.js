ceb3b72fd107907c08cc6f5d12355e4804c461be
mkfreeman <michael.k.freeman@gmail.com>
2015-11-08 19:37:38 -0800

    Prepared for exercise

--- js/app.js
+++ js/app.js
@@ -1,36 +1,13 @@
-var myApp = angular.module('myApp', ['ui.router'])
-// Config route provider
-.config(function($stateProvider) {
-    $stateProvider
-  .state('home', {
-    url:'/',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeController',
-  })
-  .state('content', {
-    url:'/content',
-    templateUrl: 'templates/content.html',
-    controller: 'ContentController',
-  })
-  .state('about', {
-    url:'/about',
-    templateUrl: 'templates/about.html',
-    controller: 'AboutController',
-  })
-})
+// Create app
+
+// Configure app
 
 // Landing page controller: define $scope.number as a number
-.controller('HomeController', function($scope){
-  $scope.number = 20
-})
+
 
 // About page controller: define $scope.about as a string
-.controller('AboutController', function($scope){
-  $scope.about = "Here's some information about this page."
-})
+
 
 // Content controller: define $scope.url as an image
-.controller('ContentController', function($scope){
-  $scope.url = "http://conference.unavsa.org/wp-content/uploads/2015/06/SEA-pic.jpg"
-})
+
 
